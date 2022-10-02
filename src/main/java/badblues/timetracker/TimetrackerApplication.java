c12711fb663a5860119097d90678478f5707303c
badbadblues _ <borovikxv.a@gmail.com>
2022-10-02 16:44:00 +0700

    connected to DB

--- src/main/java/badblues/timetracker/TimetrackerApplication.java
+++ src/main/java/badblues/timetracker/TimetrackerApplication.java
@@ -3,6 +3,8 @@ package badblues.timetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO remove username and password from database properties
+
 @SpringBootApplication
 public class TimetrackerApplication {
 

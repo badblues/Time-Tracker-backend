1b0e2b7cd134630e4469e9d8bc962e5ef20f4032
badbadblues _ <borovikxv.a@gmail.com>
2022-10-18 21:31:05 +0700

    added log4j2 configuration

--- src/main/java/badblues/timetracker/TimetrackerApplication.java
+++ src/main/java/badblues/timetracker/TimetrackerApplication.java
@@ -1,17 +1,31 @@
 package badblues.timetracker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 //TODO remove username and password from database properties
 //TODO http requests
-//TODO logs
-
+   
 @SpringBootApplication
-public class TimetrackerApplication {
+public class TimetrackerApplication implements ApplicationRunner{
+
+	private static final Logger logger = LogManager.getLogger(TimetrackerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimetrackerApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+			logger.debug("Debugging log");
+			logger.info("Info log");
+			logger.warn("Hey, This is a warning!");
+			logger.error("Oops! We have an Error. OK");
+			logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
